6cb5b6797a6431439f12a1c76a9249a6214fcd68
minlaxz <minminlaxz@gmail.com>
2021-05-14 00:55:37 +0630

    Adding custom colors.
    https://www.schemecolor.com/ios.php

---
+++ colors.js
@@ -0,0 +1,10 @@
+const colors = {
+    RadicalRed: "#FC3158",
+    MayaBlue: "#5FC9F8",
+    Sunglow: "#FECB2E",
+    CrayolaBlue: "#147EFB",
+    Emeraid: "#53D769",
+    CoralRed: "#FC3D39"
+}
+
+export default colors;
